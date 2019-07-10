3091981b706aaa68c011c05b428d031343084354
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-07-10 15:38:00 -0500

    Added tests for add method.

--- CustomListClass/CustomListClass.cs
+++ CustomListClass/CustomListClass.cs
@@ -1,7 +1,36 @@
-namespace CustomClassList
+using System;
+
+namespace CustomClassListProject
 {
     public class CustomClassList<T>
     {
+        CustomClassList<T> customClassList = new CustomClassList<T>();
+        private int count;
+        public int Count
+        {
+            get { return count; }
+        }
+        private int capacity;
+        public int Capacity
+        {
+            get { return capacity; }
+        }
         
+        public T this[int index]
+        {
+            get
+            {
+                return customClassList[index];
+            }
+            set
+            {
+                customClassList[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+
+        }
     }
 }
--- CustomListClassTest/UnitTest1.cs
+++ CustomListClassTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using CustomClassListProject;
 using NUnit.Framework;
 
 namespace Tests
@@ -9,7 +11,132 @@ namespace Tests
         {
         }
 
+        // Adding an item to an empty list, item should go to index[0] (X)
+        // Adding an item to a list with two items, item should go to index[2]
+        // Adding an item to a list should increase count by 1
+        // Adding an item to the list over capacity limit
         [Test]
+        public void Add_AddToEmptyList_ValueGoesToIndexZero()
+        {
+            // arrange
+            CustomClassList<int> test = new CustomClassList<int>();
+            int expected = 10;
+            int actual;
 
+            // act
+            test.Add(10);
+            actual = test[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_AddToListWithTwoItems_ValueGoesToIndexTwo()
+        {
+            // arrange
+            CustomClassList<int> test = new CustomClassList<int>();
+            test.Add(1);
+            test.Add(2);
+            int expected = 8;
+            int actual;
+
+            // act
+            test.Add(8);
+            actual = test[2];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_AddToListWithTwoItems_CountGoesUpByOne()
+        {
+            // arrange
+            CustomClassList<int> test = new CustomClassList<int>();
+            test.Add(1);
+            test.Add(2);
+            int expected = 3;
+            int count = 2;
+            int actual;
+
+            // act
+            test.Add(3);
+            actual = count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Indexer_GettingValueOfIndexNotExists_ThrowsError()
+        {
+            // Arrange
+            CustomClassList<int> test = new CustomClassList<int>();
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Add(4);
+            int index;
+            // Act
+            index = test[5];
+            // Assert
+            
+        }
+
+        // [Test]
+        // public void Add_AddItemOverCapacityLimit_ThrowsError()
+        // {
+        //     // arrange
+        //     CustomClassList<int> test = new CustomClassList<int>();
+        //     test.Add(1);
+        //     test.Add(2s);
+        //     test.Add(3);
+        //     test.Add(4);
+        //     test.Add(5);
+        //     bool expected = true;
+        //     bool actual;
+
+        //     // act
+        //     test.Add(6);
+        //     actual = test.isOverCapacity;
+
+        //     // assert
+        //     Assert.AreEqual(expected, actual);
+        // }
+
+        // [Test]
+        // public void Add_AddToEmptyList_CountGoesUpByOne()
+        // {
+        //     // arrange
+        //     CustomClassList<int> test = new CustomClassList<int>();
+        //     int expected = 1;
+        //     int actual;
+
+        //     // act
+        //     test.Add(1);
+        //     actual = test.Count();
+
+        //     // assert
+        //     Assert.AreEqual(expected, actual);
+        // }
+
+        // [Test]
+        // public void Add_AddItemToListWithTwoItems_ResizedList()
+        // {
+        //     // arrange
+        //     CustomClassList<int> test = new CustomClassList<int>();
+        //     test.Add(1);
+        //     test.Add(2);
+        //     int expected = 3;
+        //     int actual;
+
+        //     // act
+        //     test.Add(3);
+        //     actual = test.Count();
+
+        //     // assert
+        //     Assert.AreEqual(expected, actual);
+        // }
     }
 }
